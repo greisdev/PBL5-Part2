faa9e5f0e1794e08dd3728b0e919296289011acf
Guilherme Reis <102710518+greisdev@users.noreply.github.com>
2024-04-16 18:55:05 -0700

    MusicApp.java

--- MusicApp.java
+++ MusicApp.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 public class MusicApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int choice;
+        Playlist playlist = null; // Declare a variável fora do switch case
 
         do {
             System.out.println("\n1. Cria usuario\n2. Cria Playlist\n3. Adiciona Musica\n0. Finaliza Programa");
-            System.out.print("Enter your choice: ");
+            System.out.print("Selecionar: ");
             choice = scanner.nextInt();
 
             switch (choice) {
                 case 1:
-
                     System.out.print("Nome de usuario: ");
                     String userName = scanner.next();
                     System.out.print("Email do usuario: ");
@@ -22,24 +23,20 @@ public class MusicApp {
                     System.out.println("Usuario criado: " + user.getName());
                     break;
                 case 2:
-
-                    System.out.print("Nome de usuario: ");
-                    String userName2 = scanner.next();
-                    User user2 = new User(userName2, "");
-                    System.out.println("Usuario encontrado: " + user2.getName());
                     System.out.print("Nome da playlist: ");
                     String playlistName = scanner.next();
-                    Playlist playlist = new Playlist(playlistName);
+                    playlist = new Playlist(playlistName);
                     System.out.println("Playlist criada: " + playlist.getName());
                     break;
                 case 3:
-                    System.out.println("Nome da playlist: ");
-                    String playlistName = scanner.next();
-                    Playlist playlist = new Playlist(playlistName);
-                    System.out.println("Nome da musica: ");
-                    String songName = scanner.next();
-                    playlist.addSong(songName);
-                    playlist.printSongs();
+                    if (playlist != null) {
+                        System.out.println("Nome da musica: ");
+                        String songName = scanner.next();
+                        playlist.addSong(songName);
+                        playlist.printSongs();
+                    } else {
+                        System.out.println("Crie uma playlist primeiro.");
+                    }
                     break;
                 case 0:
                     System.out.println("Programa finalizado");
